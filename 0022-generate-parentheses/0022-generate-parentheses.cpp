ac6746b006942b38efef062abca36a57b39bb289
Hank Hsu <hank891008@gmail.com>
2023-07-15 13:28:30 +0800

    Time: 11 ms (16.10%), Space: 15.3 MB (29.69%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,30 +2,18 @@ class Solution {
 public:
     vector<string>ans;
     void dfs(string now, int l, int r, int n){
-        if(l == r && l == n){
-            int tot = 0;
-            for(int i = 0; i < now.size(); i++){
-                if(now[i] == '('){
-                    tot++;
-                }
-                else{
-                    tot--;
-                }
-                if(tot < 0){
-                    return;
-                }
-            }
-            ans.emplace_back(now);
+        if(r > l || l > n || r > n){
             return;
         }
-        if(l > n || r > n){
+        if(l == r && l == n){
+            ans.emplace_back(now);
             return;
         }
         dfs(now + "(", l + 1, r, n);
         dfs(now + ")", l, r + 1, n);
     }
     vector<string> generateParenthesis(int n) {
-        dfs("", 0, 0, n);
+        dfs("(", 1, 0, n);
         return ans;
     }
 };
