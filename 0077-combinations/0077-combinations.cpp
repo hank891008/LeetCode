f5c0e35efc5610d5d63cdf395f93b16ec4f1c9ce
Hank Hsu <hank891008@gmail.com>
2023-08-01 22:11:38 +0800

    Time: 23 ms (83.49%), Space: 9.8 MB (56.99%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
+    vector<int>tmp;
     vector<vector<int>> ans;
-    void dfs(vector<int> tmp, int used[], int n, int k, int now){
+    void dfs(int n, int k, int now){
         if(tmp.size() == k){
             ans.emplace_back(tmp);
             return;
         }
         for(int i = now; i <= n; i++){
-            if(used[i] == 0){
-                used[i] = 1;
-                tmp.emplace_back(i);
-                dfs(tmp, used, n, k, i + 1);
-                used[i] = 0;
-                tmp.pop_back();
-            }
+            tmp.emplace_back(i);
+            dfs(n, k, i + 1);
+            tmp.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
-        vector<int>tmp;
-        int used[n + 5];
-        memset(used, 0, sizeof(used));
-        dfs(tmp, used, n, k, 1);
+        dfs(n, k, 1);
         return ans;
     }
 };
