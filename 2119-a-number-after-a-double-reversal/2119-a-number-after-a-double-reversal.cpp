0a74b59860ce3312e9872fa10a6211686ab27d0c
Hank Hsu <hank891008@gmail.com>
2023-08-03 16:32:43 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (30.19%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        int tmp = num;
-        while(tmp >= 10 && tmp % 10 == 0){
-            tmp /= 10;
-        }
-        return tmp == num;
+        return (num >= 10 && num % 10 == 0) ? false : true;
     }
 };
