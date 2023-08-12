add6d81985fe18b84fcfaf95fcc159b38a69b259
Hank Hsu <hank891008@gmail.com>
2023-08-12 22:07:35 +0800

    Time: 265 ms (38.82%), Space: 62.3 MB (10.98%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
-public:
+private:
     map<pair<int, int>, int> m;
-    int solve(vector<int>&v, int idx, int target){
+    vector<int> v;
+public:
+    int solve(int idx, int target){
         if(idx == v.size()){
             return target == 0;
         }
         if(m.count({idx, target})){
             return m[{idx, target}];
         }
-        return m[{idx, target}] = solve(v, idx + 1, target - v[idx]) + solve(v, idx + 1, target + v[idx]);
+        return m[{idx, target}] = solve(idx + 1, target - v[idx]) + solve(idx + 1, target + v[idx]);
         
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums, 0, target);
+        v = nums;
+        return solve(0, target);
     }
 };
