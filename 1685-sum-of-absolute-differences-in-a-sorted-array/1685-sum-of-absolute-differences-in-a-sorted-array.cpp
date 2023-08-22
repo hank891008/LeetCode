6910a7b8e364add7190d2ed765fc5aadaa26bcc1
Hank Hsu <hank891008@gmail.com>
2023-08-23 01:46:46 +0800

    Time: 112 ms (56.48%), Space: 87.9 MB (23.80%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -8,13 +8,13 @@ public:
         for(int i = 1; i < n; i++){
             prefix[i] = prefix[i - 1] + nums[i];
         }
-        vector<int>ans;
-        ans.emplace_back(prefix[n - 1] - prefix[0] - (n - 1) * prefix[0]);
+        vector<int>result;
+        result.emplace_back(prefix[n - 1] - prefix[0] - (n - 1) * prefix[0]);
         for(int i = 1; i < n; i++){
-            int greater = (prefix[n - 1] - prefix[i]) - (n - 1 - i) * nums[i];
             int lesser = nums[i] * i - prefix[i - 1];
-            ans.emplace_back(lesser + greater);
+            int greater = (prefix[n - 1] - prefix[i]) - (n - 1 - i) * nums[i];
+            result.emplace_back(lesser + greater);
         }
-        return ans;
+        return result;
     }
 };
