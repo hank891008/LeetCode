76db692a1ac7647eb3c1a94907536d32b02fe363
Hank Hsu <hank891008@gmail.com>
2023-08-24 12:45:41 +0800

    Time: 198 ms (90.17%), Space: 41.7 MB (55.15%) - LeetHub

---
+++ 2746-decremental-string-concatenation/2746-decremental-string-concatenation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int used[1005][30][30];
+    int dfs(int idx, int s, int e, vector<string>& words){
+        if(idx == words.size()){
+            return 0;
+        }
+        if(used[idx][s][e] != -1){
+            return used[idx][s][e];
+        }
+        int first = words[idx].front() - 'a';
+        int last = words[idx].back() - 'a';
+        int minn = INT_MAX;
+        minn = min(minn, (int)words[idx].size() - (e == first) + dfs(idx + 1, s, last, words));
+        minn = min(minn, (int)words[idx].size() - (s == last) + dfs(idx + 1, first, e, words));
+        return used[idx][s][e] = minn;
+        
+    }
+    int minimizeConcatenatedLength(vector<string>& words) {
+        memset(used, -1, sizeof(used));
+        return words[0].size() + dfs(1, words[0].front() - 'a', words[0].back() - 'a', words);
+    }
+};
