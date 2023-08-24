ba2a031a6eaf2374091b05949aa628ff9da9e375
Hank Hsu <hank891008@gmail.com>
2023-08-24 14:00:04 +0800

    Time: 0 ms (100.00%), Space: 9.8 MB (7.12%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    bool dp(int idx, vector<int>& bits){
-        if(idx < 0){
-            return true;
-        }
-        bool ret = false;
-        if(bits[idx] == 0){
-            ret |= dp(idx - 1, bits);
-        }
-        if(idx >= 1 && bits[idx - 1] == 1){
-            ret |= dp(idx - 2, bits);
-        }
-        return ret;
-    }
     bool isOneBitCharacter(vector<int>& bits) {
-        if(bits.size() == 1){
-            return true;
+        int i = 0;
+        while(i < bits.size() - 1){
+            if(bits[i] == 0){
+                i++;
+            }
+            else{
+                i += 2;
+            }
         }
-        return dp((int)bits.size() - 2, bits); 
+        return i == bits.size() - 1;
     }
 };
