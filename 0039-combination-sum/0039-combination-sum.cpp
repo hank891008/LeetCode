234a8692608da5cbf7ff8fd720f6908a393457c1
Hank Hsu <hank891008@gmail.com>
2023-09-01 21:59:51 +0800

    Time: 4 ms (78.52%), Space: 12.5 MB (42.42%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    set<vector<int>>s;
-    void dfs(vector<int>& v, vector<int> tmp, int target){
+    void dfs(vector<int>& v, int idx, vector<int> tmp, int target){
         if(target == 0){
-            sort(tmp.begin(), tmp.end());
-            if(s.count(tmp)){
-                return;
-            }
-            s.insert(tmp);
             ans.emplace_back(tmp);
             return;
         }
-        for(int i = 0; i < v.size(); i++){
+        for(int i = idx; i < v.size(); i++){
             if(target - v[i] >= 0){
                 tmp.emplace_back(v[i]);
-                dfs(v, tmp, target - v[i]);
+                dfs(v, i, tmp, target - v[i]);
                 tmp.pop_back();
             }
         }
@@ -23,7 +17,7 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.rbegin(), candidates.rend());
         vector<int>tmp;
-        dfs(candidates, tmp, target);
+        dfs(candidates, 0, tmp, target);
         return ans;
     }
 };
