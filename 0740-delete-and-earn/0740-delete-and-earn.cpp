bb6a02e6cf8e6f15e1d04e904cef84bc1e946519
Hank Hsu <hank891008@gmail.com>
2024-01-25 20:05:55 +0800

    Time: 3 ms (98.87%), Space: 18.7 MB (11.45%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -3,13 +3,16 @@ public:
     int deleteAndEarn(vector<int>& nums) {
         int MAXN = 2e4 + 5;
         vector<int> v(MAXN);
+        int minn = INT_MAX, maxx = INT_MIN;
         for(int i = 0; i < nums.size(); i++){
             v[nums[i]]++;
+            minn = min(minn, nums[i]);
+            maxx = max(maxx, nums[i]);
         }
         v[2] = 2 * v[2];
-        for(int i = 3; i < MAXN; i++){
+        for(int i = max(3, minn); i <= maxx; i++){
             v[i] = i * v[i] + max(v[i - 2], v[i - 3]);
         }
-        return max(v[MAXN - 1], v[MAXN - 2]);
+        return max(v[maxx], v[maxx - 1]);
     }
 };
