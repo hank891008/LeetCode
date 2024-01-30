2eade748430ed151cdf55135864f3b0eadd37e03
Hank Hsu <hank891008@gmail.com>
2024-01-30 15:03:16 +0800

    Time: 8 ms (94.32%), Space: 20.4 MB (8.60%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            while(nums[i] < nums.size() && nums[i] != nums[nums[i]]){
+                swap(nums[i], nums[nums[i]]);
+            }
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(i != nums[i]){
+                return i;
+            }
+        }
+        return nums.size();
+    }
+};
