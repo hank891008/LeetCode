5b47eec88104da906141da260901ba0bea7d7194
Hank Hsu <hank891008@gmail.com>
2024-02-08 17:30:04 +0800

    Time: 234 ms (75.77%), Space: 83.6 MB (29.65%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -3,19 +3,19 @@ public:
     const int MAXN = 0x3f3f3f3f;
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> ans(n, vector<int>(m, MAXN));
+        vector<vector<int>> ans(2, vector<int>(m, MAXN));
         for(int i = 0; i < m; i++){
             ans[0][i] = grid[0][i];
         }
         int ret = MAXN;
         for(int i = 1; i < n; i++){
             for(int j = 0; j < m; j++){
+                ans[i % 2][j] = MAXN;
                 for(int k = 0; k < m; k++){
-                    int tmp = grid[i][j] + ans[i - 1][k] + moveCost[grid[i - 1][k]][j];
-                    ans[i][j] = min(ans[i][j], tmp);
+                    ans[i % 2][j] = min(ans[i % 2][j], grid[i][j] + ans[(i - 1) % 2][k] + moveCost[grid[i - 1][k]][j]);
                 }
                 if(i == n - 1){
-                    ret = min(ret, ans[i][j]);
+                    ret = min(ret, ans[i % 2][j]);
                 }
             }
         }
