fe04328f553050def7a9974c4c8ac0e4d797e37c
Hank Hsu <hank891008@gmail.com>
2024-02-10 18:45:09 +0800

    Time: 7 ms (94.77%), Space: 19.4 MB (28.04%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        for(auto l: letters){
+            if(l > target){
+                return l;
+            }
+        }
+        return letters[0];
+    }
+};
