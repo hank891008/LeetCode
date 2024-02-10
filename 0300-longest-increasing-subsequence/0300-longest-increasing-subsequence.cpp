5370030fd9979a3ba1061569bf7a10a6a200191e
Hank Hsu <hank891008@gmail.com>
2024-02-10 18:10:00 +0800

    Time: 9 ms (77.04%), Space: 12.7 MB (29.41%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>ans = {-20000};
+        vector<int>ans;
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] > ans.back()){
+            int idx = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+            if(idx == ans.size()){
                 ans.emplace_back(nums[i]);
             }
-            for(int j = ans.size() - 1; j >= 0; j--){
-                if(ans[j] < nums[i]){
-                    ans[j + 1] = min(ans[j + 1], nums[i]);
-                }
+            else{
+                ans[idx] = nums[i];
             }
         }
-        return ans.size() - 1;
+        return ans.size();
     }
 };
