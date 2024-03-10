f9db022a5b65c3498969e0814dc39a6096207616
Hank Hsu <hank891008@gmail.com>
2024-03-10 20:15:52 +0800

    Time: 194 ms (67.30%), Space: 119.7 MB (60.84%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        int size = candyType.size();
-        unordered_set<int> m(candyType.begin(), candyType.end());
-        return min(size / 2, (int)m.size());
+        return min(candyType.size() / 2, unordered_set<int> (candyType.begin(), candyType.end()).size());
     }
 };
