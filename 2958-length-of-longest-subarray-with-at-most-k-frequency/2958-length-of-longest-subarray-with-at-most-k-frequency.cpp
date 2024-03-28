18f95868c98d34f911a1c5d2eb1ca5d611a723ea
Hank Hsu <hank891008@gmail.com>
2024-03-29 00:06:31 +0800

    Time: 240 ms (29.15%), Space: 150.2 MB (7.75%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,7 +4,7 @@ public:
         int l = 0, r = 0;
         int ans = 0;
         unordered_map<int, int> m;
-        while(l < nums.size() && r < nums.size()){
+        while(r < nums.size()){
             if(!m.count(nums[r]) || m[nums[r]] < k){
                 m[nums[r]]++;
                 ans = max(ans, r - l + 1);
