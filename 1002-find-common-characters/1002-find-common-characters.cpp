cfe260b0cd9cc39ded9ea9d5999029c8a53f3d65
Hank Hsu <hank891008@gmail.com>
2024-06-07 00:24:30 +0800

    Time: 9 ms (49.58%), Space: 11.4 MB (60.76%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> alpha(26, 105);
+        for(int i = 0; i < words.size(); i++){
+            vector<int> tmp(26, 0); 
+            for(int j = 0; j < words[i].size(); j++){
+                tmp[words[i][j] - 'a']++;
+            }
+            for(int j = 0; j < 26; j++){
+                alpha[j] = min(alpha[j], tmp[j]);
+            }
+        }
+        vector<string> ans;
+        for(int i = 0; i < 26; i++){
+            if(alpha[i] != 0){
+                for(int j = 0; j < alpha[i]; j++){
+                    ans.emplace_back(string(1, 'a' + i));
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
