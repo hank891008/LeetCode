94372e3e945e59864f95fad8a71fa39f011a758f
許瀚丰 <hank891008@gmail.com>
2024-09-19 22:51:30 +0800

    Time: 12 ms (16.08%), Space: 15.4 MB (94.63%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    static bool cmp(int a, int b){
-        string aa = to_string(a);
-        string bb = to_string(b);
-        return (aa + bb) >= (bb + aa);
-    }
     string largestNumber(vector<int>& nums) {
-        int flag = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0){
-                flag++;
-            }
+        sort(nums.begin(), nums.end(), [](int x, int y){
+            return to_string(x) + to_string(y) > to_string(y) + to_string(x);
+        });
+        string ans;
+        for(auto num: nums){
+            ans += to_string(num);
         }
-        if(flag == 0){
+        if(ans[0] == '0'){
             return "0";
         }
-        string ret;
-        sort(nums.begin(), nums.end(), cmp);
-        for(int i = 0; i < nums.size(); i++){
-            ret += to_string(nums[i]);
-        }
-        return ret;
+        return ans;
     }
 };
