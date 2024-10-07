97d024dd51ad552853bd81b213d0142f91f4032b
許瀚丰 <hank891008@gmail.com>
2024-10-07 13:48:33 +0800

    Time: 40 ms (27.52%), Space: 16.8 MB (16.22%) - LeetHub

--- 1175-prime-arrangements/1175-prime-arrangements.py
+++ 1175-prime-arrangements/1175-prime-arrangements.py
@@ -2,5 +2,10 @@ class Solution:
     def numPrimeArrangements(self, n: int) -> int:
         prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
         MOD = 1000000007
+        @cache
+        def factorial(x):
+            return 1 if x <= 1  else ((x % MOD) * (factorial(x - 1) % MOD)) % MOD
+        
         num_of_p = sum(list(map(lambda x: x in prime, range(2, n + 1))))
-        return (math.factorial(num_of_p) * math.factorial(n - num_of_p)) % MOD
+        
+        return ((factorial(num_of_p) % MOD) * (factorial(n - num_of_p) % MOD)) % MOD
