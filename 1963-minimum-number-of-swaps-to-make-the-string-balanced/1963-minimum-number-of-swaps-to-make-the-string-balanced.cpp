05c82531b6219ffe7b6182f9e903506401e1c463
許瀚丰 <hank891008@gmail.com>
2024-10-09 16:35:23 +0800

    Time: 98 ms (79.56%), Space: 32.7 MB (78.55%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -3,8 +3,7 @@ public:
     int minSwaps(string s) {
         int cnt = 0;
         for(int i = 0; i < s.size(); i++){
-            cnt += (s[i] == '[');
-            cnt = max(0, cnt - (s[i] == ']'));
+            cnt = max(0, cnt + (s[i] == '[') - (s[i] == ']'));
         }
         return (cnt + 1) / 2;
     }
