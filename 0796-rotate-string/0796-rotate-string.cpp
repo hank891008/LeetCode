43d75d9e06a5d1da51937edb02afb7bdd8cab426
許瀚丰 <hank891008@gmail.com>
2024-11-07 00:03:51 +0800

    Time: 0 ms (100.00%), Space: 7.7 MB (57.23%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size() != goal.size()){
+            return false;
+        }
+        int n = s.size();
+        for(int i = 0; i < n; i++){
+            int flag = true;
+            for(int j = 0; j < n; j++){
+                if(s[(i + j) % n] != goal[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                return true;
+            }
+        }
+        return false;
+    }
+};
