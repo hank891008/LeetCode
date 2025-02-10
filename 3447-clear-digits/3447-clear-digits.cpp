6e601b4bf0e93def939b4e05c501724ba4d5e64b
許瀚丰 <hank891008@gmail.com>
2025-02-10 23:17:07 +0800

    Time: 0 ms (100%), Space: 8.4 MB (70.42%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans;
+        for(int i = 0; i < s.size(); i++){
+            if(isalpha(s[i])){
+                ans += s[i];
+            }
+            else{
+                ans.pop_back();
+            }
+        }
+        return ans;
+
+    }
+};
