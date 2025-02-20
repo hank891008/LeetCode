8ddfc86f6a456ba884457b08c48fc21cf994e0f7
許瀚丰 <hank891008@gmail.com>
2025-02-21 00:47:03 +0800

    Time: 0 ms (100%), Space: 12.3 MB (99.65%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        string ans;
         for(int i = 0; i < nums.size(); i++){
-            ans += (nums[i][i] == '0') ? '1' : '0';
+            nums[0][i] = (nums[i][i] == '0') ? '1' : '0';
         }
-        return ans;
+        return nums[0];
     }
 };
