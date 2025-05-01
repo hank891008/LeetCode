68ba249b5e43fb2b6d9ab2ce9570a89874749326
許瀚丰 <hank891008@gmail.com>
2025-05-01 23:28:40 +0800

    Time: 48 ms (75.81%), Space: 33.3 MB (27.92%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.cpp
@@ -1,37 +1,53 @@
 class Solution {
 public:
     vector<vector<int>> G;
-    vector<bool> used;
-    void dfs(int u){
-        used[u] = true;
-        for(int v: G[u]){
-            if(!used[v]){
-                dfs(v);
+    vector<int> used;
+    int tag = 0;
+
+    int bfs(int start) {
+        int cnt = 0;
+        queue<int> q{{start}};
+        used[start] = tag;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            cnt++;
+
+            for (int v : G[u]) {
+                if (used[v] != tag) {
+                    used[v] = tag;
+                    q.push(v);
+                }
             }
         }
+
+        return cnt;
     }
     int maximumDetonation(vector<vector<int>>& bombs) {
         int n = bombs.size();
         G.resize(n);
-        for(int i = 0; i < bombs.size(); i++){
-            for(int j = i + 1; j < bombs.size(); j++){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
                 long long x1 = bombs[i][0], y1 = bombs[i][1], r1 = bombs[i][2];
-                long long x2 = bombs[j][0], y2 = bombs[j][1], r2 = bombs[j][2];
-                long long dis = abs(x1 - x2) * abs(x1 - x2) + abs(y1 - y2) * abs(y1 - y2);
-                if(dis <= r1 * r1){
+                long long x2 = bombs[j][0], y2 = bombs[j][1];
+                long long dis = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+                if (dis <= r1 * r1) {
                     G[i].emplace_back(j);
-                }
-                if(dis <= r2 * r2){
-                    G[j].emplace_back(i);
                 }
             }
         }
+
         int maxx = 0;
-        for(int i = 0; i < n; i++){
-            used.assign(n, false);
-            dfs(i);
-            maxx = max(maxx, accumulate(used.begin(), used.end(), 0));
+        used.assign(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            tag++;
+            maxx = max(maxx, bfs(i));
         }
+
         return maxx;
     }
 };
