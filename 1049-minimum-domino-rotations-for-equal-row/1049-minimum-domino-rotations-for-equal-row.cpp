6ae4baafd10e736ed62cd3ea787e25d0087ac21f
許瀚丰 <hank891008@gmail.com>
2025-05-03 15:51:56 +0800

    Time: 0 ms (100%), Space: 115.3 MB (93.75%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     pair<int, int> solve(vector<int>& a, vector<int>& b){
         int cnt1 = 0, cnt2 = 1;
-        int now1 = a[0], now2 = b[0];
         for(int i = 1; i < a.size(); i++){
-            if(cnt1 != INT_MAX && a[i] != now1){
-                if(b[i] == now1){
+            if(cnt1 != INT_MAX && a[i] != a[0]){
+                if(b[i] == a[0]){
                     cnt1++;
                 }
                 else{
                     cnt1 = INT_MAX;
                 }
             }
-            if(cnt2 != INT_MAX && a[i] != now2){
-                if(b[i] == now2){
+            if(cnt2 != INT_MAX && a[i] != b[0]){
+                if(b[i] == b[0]){
                     cnt2++;
                 }
                 else{
