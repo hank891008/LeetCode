e44b6d46ef9645b2699da42d594b04f2347758f0
許瀚丰 <hank891008@gmail.com>
2025-05-04 21:55:56 +0800

    Time: 0 ms (100%), Space: 26.2 MB (48.5%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -4,10 +4,7 @@ public:
         int ans = 0;
         unordered_map<int, int> mp;
         for(auto& d: dominoes){
-            if(d[0] > d[1]){
-                swap(d[0], d[1]);
-            }
-            ans += (++mp[d[0] * 10 + d[1]] - 1);
+            ans += (++mp[min(d[0], d[1]) * 10 + max(d[0], d[1])] - 1);
             
         }
         return ans;
