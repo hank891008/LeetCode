4e95bc41cc9f12e2d5c07c21b4e6b126c8dc4c0a
許瀚丰 <hank891008@gmail.com>
2025-05-05 11:14:15 +0800

    Time: 96 ms (97%), Space: 251.7 MB (79.46%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -0,0 +1,34 @@
+class NumberContainers {
+public:
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> mp;
+    unordered_map<int, int> table;
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        table[index] = number;
+        mp[number].emplace(index);
+    }
+    
+    int find(int number) {
+        if(!mp.count(number)){
+            return -1;
+        }
+        while(!mp[number].empty()){
+            int idx = mp[number].top();
+            if(table[idx] == number){
+                return idx;
+            }
+            mp[number].pop();
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
