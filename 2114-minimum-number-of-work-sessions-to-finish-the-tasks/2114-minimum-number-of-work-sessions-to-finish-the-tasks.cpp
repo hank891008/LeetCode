62857a28e08400c0e3fb17056cdfabf771051946
許瀚丰 <hank891008@gmail.com>
2025-07-29 10:07:42 -0700

    Time: 27 ms (75.87%), Space: 10.7 MB (84.58%) - LeetHub

--- 2114-minimum-number-of-work-sessions-to-finish-the-tasks/2114-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ 2114-minimum-number-of-work-sessions-to-finish-the-tasks/2114-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
+    int ans = 16;
+    void dfs(vector<int>& tasks, vector<int>& sessions, int i, int sessionTime){
+        if(sessions.size() >= ans) return;
+        if(i == tasks.size()){
+            ans = sessions.size();
+            return;
+        }
+        for(int j = 0; j < sessions.size(); j++){
+            if(sessions[j] + tasks[i] <= sessionTime){
+                sessions[j] += tasks[i];
+                dfs(tasks, sessions, i + 1, sessionTime);
+                sessions[j] -= tasks[i];
+            }
+        }
+        sessions.emplace_back(tasks[i]);
+        dfs(tasks, sessions, i + 1, sessionTime);
+        sessions.pop_back();
+    }
     int minSessions(vector<int>& tasks, int sessionTime) {
-        int n = tasks.size();
-        vector<int> check(1 << n, false);
-        for(int i = 1; i < (1 << n); i++){
-            int total = 0;
-            for(int j = 0; j < n; j++){
-                if((i >> j) & 1){
-                    total += tasks[j];
-                }
-            }
-            check[i] = total <= sessionTime;
-        }
-
-        vector<int> dp(1 << n, 0x3f3f3f3f);
-        dp[0] = 0;
-        for(int i = 1; i < (1 << n); i++){
-            for(int j = i; j; j = (j - 1) & i){
-                if(check[j]){
-                    dp[i] = min(dp[i], dp[i ^ j] + 1);
-                }
-            }
-        }
-        return dp[(1 << n) - 1];
+        vector<int> s;
+        dfs(tasks, s, 0, sessionTime);
+        return ans;
     }
 };
