d6f0bf89146c9ab0493543ddb85ade16ee09fe28
許瀚丰 <hank891008@gmail.com>
2026-01-03 15:39:00 +0800

    Time: 0 ms (100%), Space: 28.4 MB (69.81%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(auto num: nums) {
+            if(++mp[num] > 1) {
+                return num;
+            }
+        }
+        return -1;
+    }
+};
