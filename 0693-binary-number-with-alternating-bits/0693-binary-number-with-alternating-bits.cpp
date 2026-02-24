53acb2016582406a3432075ee919ee22675b614f
許瀚丰 <hank891008@gmail.com>
2026-02-24 13:32:23 +0800

    Time: 0 ms (100%), Space: 7.6 MB (90.92%) - LeetHub

---
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int prev = !(n & 1);
+        while(n) {
+            if(prev == (n & 1)){
+                return false;
+            }
+            prev = n & 1;
+            n >>= 1;
+        }
+        return true;
+    }
+};
